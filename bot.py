19a36061c9d6507abcd9ad12436fcb40b5a4667a
hguubhhkjnbh-netizen <hguubhhkjnbh@gmail.com>
2026-04-22 22:09:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,10 @@ conn.commit()
 
 # Check admin
 async def is_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
-member = await context.bot.get_chat_member(update.effective_chat.id, update.effective_user.id)
+member = await context.bot.get_chat_member(
+update.effective_chat.id,
+update.effective_user.id
+)
 return member.status in ["administrator", "creator"]
 
 # gmute command
